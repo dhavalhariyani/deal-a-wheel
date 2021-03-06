886a80821d8320e58d44d115a5f32caa9663c88a
Dhaval Hariyani <haryanidhaval6@gmail.com>
2021-03-06 16:31:06 +0530

    Update db_conn.php

--- admin/db_conn.php
+++ admin/db_conn.php
@@ -1,11 +1,21 @@
-<?php 
 
- $db = new mysqli("localhost","root","");
+
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["b7500849b9e969"];
+$cleardb_password = $cleardb_url["c361994a"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+ $db = new mysqli($cleardb_db,$cleardb_username, $cleardb_password);
    if($db->connect_errno > 0){
          die('Unable to connect to database [' . $db->connect_error . ']');  } 
-     
-	 $db->query("CREATE DATABASE IF NOT EXISTS `deal-a-wheel`");
-	 
-             mysqli_select_db($db,"deal-a-wheel");
 
- ?>
+             mysqli_select_db($db,$cleardb_db);
+
+?>
