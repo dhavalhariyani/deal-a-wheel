5ae648cd6f242e3e0966a8683533755541f7241f
Dhaval Hariyani <haryanidhaval6@gmail.com>
2021-04-29 18:12:26 +0530

    Update db_conn.php

--- user/db_conn.php
+++ user/db_conn.php
@@ -1,10 +1,10 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
-$cleardb_username = $cleardb_url["b059e439a2fe34"];
-$cleardb_password = $cleardb_url["b0fd0555"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
