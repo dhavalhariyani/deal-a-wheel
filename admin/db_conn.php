b9e6610caf12f1159508c64602046be9410ab2ca
Dhaval Hariyani <haryanidhaval6@gmail.com>
2021-04-29 18:24:05 +0530

    Update db_conn.php

--- admin/db_conn.php
+++ admin/db_conn.php
@@ -1,15 +1,12 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_server   = "us-cdbr-east-03.cleardb.com";
-$cleardb_username = "b059e439a2fe34";
-$cleardb_password = "b0fd0555";
-$cleardb_db       = "heroku_b71b87e8ebd3f16";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
-$query_builder = TRUE;
 // Connect to DB
-//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
  $db = new mysqli($cleardb_db,$cleardb_username, $cleardb_password);
  mysqli_select_db($db,$cleardb_db);
-
 ?>
